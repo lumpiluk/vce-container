8bcc45d1fc249ba9a0d1c816d7a43cecce51d804
Lukas Stratmann <stratmann@ccs-labs.org>
2023-03-20 13:52:08 +0100

    Workaround for OMNeT++ IDE's error.log file that wasn't writable

--- shell.sh
+++ shell.sh
@@ -2,4 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-apptainer shell $SCRIPT_DIR/vce-container.sif
+# We need some writable directory for the OMNeT++ IDE to store its error.log file:
+OMNETPP_IDE_LOG=$HOME/omnetpp/log
+mkdir -p $OMNETPP_IDE_LOG
+
+apptainer shell --bind $OMNETPP_IDE_LOG:/log $SCRIPT_DIR/vce-container.sif
